634e997e7560b43b1a91954ff978ea6d038ae51a
Pedro G. Perez <pedrogperezalvarez@gmail.com>
2022-09-22 23:25:46 -0700

    tailwinds practice

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [/** @type {import('tailwindcss').Config} */
+  module.exports = {
+    content: [
+      "./pages/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      colors: {
+      },
+      extend: {},
+    },
+    plugins: [require('@tailwindcss/forms'),],
+  }],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
